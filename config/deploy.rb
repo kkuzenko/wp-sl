9951a0124203d3b6966a45af05e6b95c0f4985e3
kkuzenko <kuzenko@gmail.com>
2015-09-25 17:27:23 +0300

    clean up

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,17 +8,17 @@ require './config/slack'
 # Setup WordPress
 ############################################
 
-set :wp_user, "yourname" # The admin username
-set :wp_email, "yourname@example.com" # The admin email address
-set :wp_sitename, "WP Deploy" # The site title
-set :wp_localurl, "http://wpdeploy" # Your local environment URL
+set :wp_user, "kuzenko" # The admin username
+set :wp_email, "kuzenko@gmail.com" # The admin email address
+set :wp_sitename, "SLCOM" # The site title
+set :wp_localurl, "http://localhost.kk" # Your local environment URL
 
 ############################################
 # Setup project
 ############################################
 
-set :application, "wp-deploy"
-set :repo_url, "git@github.com:Mixd/wp-deploy.git"
+set :application, "wp-sl"
+set :repo_url, "git@github.com:kkuzenko/wp-sl.git"
 set :scm, :git
 
 set :git_strategy, SubmoduleStrategy
