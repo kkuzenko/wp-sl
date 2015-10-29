f2c50ecfcae5ba98baafe225c58c404806f79236
cmajorsmith <cmajorsmith@gmail.com>
2015-10-29 15:15:44 +0200

    gir commit 2

---
+++ content/plugins/wp-orphanage-extended/wp-orphanage-extended-options.php
@@ -0,0 +1,71 @@
+<?php
+	if ( ! current_user_can( 'manage_options' ) ) {
+		wp_die( __( 'You do not have sufficient permissions to access this page.', WPOEX_TD ) );
+	}
+
+
+	// Update options
+	if ( isset( $_POST['action'] ) && $_POST['action'] == 'update' ) {
+		update_option( 'wporphanageex_role', $_POST['wporphanageex_role'] );
+		if ( isset( $_POST['wporphanageex_prefixes'] ) && is_array( $_POST['wporphanageex_prefixes'] ) ) {
+			$prefixes = array();
+			foreach ( $_POST['wporphanageex_prefixes'] as $prefix ) {
+				if ( ! empty( $prefix ) ) {
+					$prefixes[] = $prefix;
+				}
+			}
+
+			update_option( 'wporphanageex_prefixes', $prefixes );
+		}
+
+		echo '<div class="updated"><p><strong>' . __( 'Settings saved', WPOEX_TD ) . '</strong></p></div>';
+	}
+
+$roles = wporphanageex_get_roles();
+$wp_orphanageex_role = get_option( 'wporphanageex_role' );
+$prefixes = get_option( 'wporphanageex_prefixes' );
+
+?>
+<div class="wrap">
+	<?php screen_icon(); ?>
+	<h2><?php _e( 'WP Orphanage Extended', WPOEX_TD ); ?></h2>
+
+	<form method="post" action="" id="wporphanageex-settings">
+		<input type="hidden" name="action" value="update" />
+
+		<table class="form-table">
+			<tr valign="top">
+				<th scope="row"><label for="wporphanageex_role"><?php _e( 'Choose Default Role:', WPOEX_TD ); ?></label></th>
+				<td>
+					<select name="wporphanageex_role" id="wporphanageex_role">
+						<?php if ( $roles ) : ?>
+							<?php foreach ( $roles as $role => $value ) : ?>
+								<option value="<?php echo $role; ?>" <?php selected( $wp_orphanageex_role, $role ); ?>><?php echo ucfirst( $role ); ?></option>
+							<?php endforeach; ?>
+						<?php endif; ?>
+					</select><br />
+					<small><?php _e( 'Choose the default role orphan users should be promoted to (if no role to copy from other table was found).', WPOEX_TD ); ?></small>
+				</td>
+			</tr>
+			<tr valign="top">
+				<th scope="row"><label for="wporphanageex_prefixes"><?php _e( 'Add WP Prefixes:', WPOEX_TD ); ?></label></th>
+				<td>
+					<?php if ( $prefixes ) : ?>
+						<?php $i = 1; ?>
+						<?php foreach ( $prefixes as $prefix ) : ?>
+							<?php _e( 'Prefix', WPOEX_TD ); ?> <?php echo $i; ?>: <input name="wporphanageex_prefixes[]" id="wporphanageex_prefixes_<?php echo $i; ?>" class="regular-text" type="text" value="<?php echo $prefix; ?>" /><br />
+							<?php $i++; ?>
+						<?php endforeach; ?>
+					<?php endif; ?>
+					<br /><?php _e( 'Add new:', WPOEX_TD ); ?> <input name="wporphanageex_prefixes[]" id="wporphanageex_prefixes" class="regular-text" type="text" value="" /><br />
+					<small><?php _e( 'Add prefixes of all WP installs where to search for user role. To remove field, leave it empty. Default WP prefix is <code>wp_</code> ', WPOEX_TD ); ?></small>
+				</td>
+			</tr>
+		</table>
+
+		<p class="submit">
+			<input type="submit" name="submit" value="<?php _e( 'Save Changes', WPOEX_TD ); ?>" class="button-primary" />
+		</p>
+
+	</form>
+</div>
---
+++ content/plugins/wp-orphanage-extended/wp-orphanage-extended.php
@@ -0,0 +1,171 @@
+<?php
+/*
+	Plugin Name: WP-Orphanage Extended
+	Plugin URI: http://blog.meloniq.net/2012/01/29/wp-orphanage-extended/
+	Description: Plugin to promote users with no roles set (the orphans) to the role from other blog where they registered or to default if any found.
+	Author: MELONIQ.NET
+	Version: 1.1
+	Author URI: http://meloniq.net/
+*/
+
+
+/**
+ * Avoid calling file directly
+ */
+if ( ! function_exists( 'add_action' ) ) {
+	die( 'Whoops! You shouldn\'t be doing that.' );
+}
+
+
+/**
+ * Plugin version and textdomain constants
+ */
+define( 'WPOEX_VERSION', '1.1' );
+define( 'WPOEX_TD', 'wp-orphanage-extended' );
+
+
+/**
+ * Process actions on plugin activation
+ */
+register_activation_hook( plugin_basename( __FILE__ ), 'wporphanageex_activate' );
+
+
+/**
+ * Load Text-Domain
+ */
+load_plugin_textdomain( WPOEX_TD, false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+
+
+/**
+ * Action on plugin activate
+ */
+function wporphanageex_activate() {
+	global $wpdb;
+
+	// set default role if not exist
+	if ( ! get_option( 'wporphanageex_role' ) && get_option( 'default_role' ) ) {
+		update_option( 'wporphanageex_role', get_option('default_role') );
+	} else {
+		update_option( 'wporphanageex_role', 'subscriber' );
+	}
+
+	// set default prefix if not exist
+	$prefixes = array();
+	$prefixes[] = $wpdb->prefix;
+	if ( ! get_option( 'wporphanageex_prefixes' ) ) {
+		update_option( 'wporphanageex_prefixes', $prefixes );
+	}
+
+}
+
+
+/**
+ * Populate administration menu of the plugin
+ */
+function add_wporphanageex_options_page() {
+
+	add_options_page( __( 'WP Orphanage Extended', WPOEX_TD ), __( 'WP Orphanage Extended', WPOEX_TD ), 'administrator', 'wp-orphanage-extended', 'wporphanageex_menu_settings' );
+}
+add_action( 'admin_menu', 'add_wporphanageex_options_page' );
+
+
+/**
+ * Create settings page in admin
+ */
+function wporphanageex_menu_settings() {
+	include_once( dirname( __FILE__ ) . '/wp-orphanage-extended-options.php' );
+}
+
+
+/**
+ * Adopts orphaned user
+ *
+ * @param string $login
+ *
+ * @return void
+ */
+function wporphanageex_adopt_this_orphan( $login ) {
+  $user = get_user_by( 'login', $login );
+
+	if ( ! current_user_can( 'read' ) ) {
+		$user_up = new WP_User( $user->ID );
+		$user_up->set_role( wporphanageex_search_user_role( $user->ID ) );
+	}
+}
+add_action( 'wp_login', 'wporphanageex_adopt_this_orphan' ); 
+
+
+/**
+ * Adopts all orphaned users
+ *
+ * @return void
+ */
+function wporphanageex_adopt_all_orphans() {
+	foreach ( wporphanageex_get_all_users() as $user_id ) {
+		$user = new WP_User( $user_id );
+		if ( ! user_can( $user_id, 'read' ) ) {
+			$user->set_role( wporphanageex_search_user_role( $user_id ) );
+		}
+	}
+}
+add_action( 'load-users.php', 'wporphanageex_adopt_all_orphans' );
+
+
+/**
+ * Returns an array of user roles
+ *
+ * @return array
+ */
+function wporphanageex_get_roles() {
+	global $wpdb;
+
+	$option = $wpdb->prefix . 'user_roles';
+	return get_option( $option );
+}
+
+
+/**
+ * Returns an array of user IDs
+ *
+ * @return array
+ */
+function wporphanageex_get_all_users() {
+	global $wpdb;
+
+	$results = $wpdb->get_col( "SELECT ID FROM $wpdb->users" );
+	return $results;
+}
+
+
+/**
+ * Searching other blogs and returns a user role, if not found, returns default one
+ *
+ * @param int $user_id (optional)
+ *
+ * @return string
+ */
+function wporphanageex_search_user_role( $user_id = false ) {
+	global $wpdb, $current_user;
+
+	$current_user = wp_get_current_user();
+	if ( ! $user_id ) {
+		$user_id = $current_user->ID;
+	}
+
+	$prefixes = get_option( 'wporphanageex_prefixes' );
+	if ( $prefixes && is_array( $prefixes ) ) {
+		foreach ( $prefixes as $prefix ) {
+			$role = get_user_meta( $user_id, $prefix . 'capabilities', true );
+			if ( $role != '' && is_array( $role ) ) {
+				foreach ( $role as $key => $value ) {
+					return $key;
+				}
+			}
+		}
+	}
+
+	// if no one was found, return default role
+	$default = get_option( 'wporphanageex_role' );
+	return $default;
+}
+
