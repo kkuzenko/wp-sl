902dc0edb0681dc096c330b5c0a1cd680c24b517
cmajorsmith <cmajorsmith@gmail.com>
2015-11-06 21:11:13 +0200

    ...

---
+++ content/themes/liteladder/taxonomy-events.php
@@ -0,0 +1,8 @@
+<?php
+var_dump(get_terms('events'));
+$term_id = get_queried_object()->term_id;
+$event_meta = json_decode(get_option('event_meta_'. $term_id),true);
+
+$path = get_template_directory();
+$path .= '/events-tpl/'. $event_meta['shortname'] . '.php';
+require_once($path);
